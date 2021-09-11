4a09b23c8593fc73024a5c04b0aa4d1eb3242532
vaniilka <mqvp@mail.ru>
2021-09-11 23:53:37 +0300

    Working code
    It's left to make pretty output and that's all.

--- v2.py
+++ v2.py
@@ -3,9 +3,11 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import pandas as pd
+import datetime
 
 class exercise():
-    def __init__(self, date, day, time, number, location, subject, type, format, group, lecture):
+    def __init__(self):
         self.date = ""
         self.day = ""
         self.time = ""
@@ -15,8 +17,17 @@ class exercise():
         self.type = ""
         self.format = ""
         self.group = ""
-        self.lecture = ""
+        self.teacher = ""
 
+    def __str__(self):
+        return f'{self.location}\n{self.time} {self.type}\n{self.subject}'
+
+
+people = ['Кокова Полина Дмитриевна', 'Шерман Лея Александровна']
+weekly_keys = ['пн1', 'пн2', 'пн3', 'пн4', 'пн5', 'пн6', 'пн7', 'вт1', 'вт2', 'вт3', 'вт4', 'вт5', 'вт6', 'вт7', 'ср1',
+               'ср2', 'ср3', 'ср4', 'ср5', 'ср6', 'ср7', 'чт1', 'чт2', 'чт3',
+               'чт4', 'чт5', 'чт6', 'чт7', 'пт1', 'пт2', 'пт3', 'пт4', 'пт5', 'пт6', 'пт7', 'сб1', 'сб2', 'сб3', 'сб4',
+               'сб5', 'сб6', 'сб7', 'вс1', 'вс2', 'вс3', 'вс4', 'вс5', 'вс6', 'вс7']
 
 PATH = '/Users/marklitvinov/Documents/GitHub/hse_timetable/chromedriver'
 option = webdriver.ChromeOptions()
@@ -24,27 +35,74 @@ option.add_argument("-incognito")
 
 driver = webdriver.Chrome(PATH, options=option)
 
-#while True:
+arr = []  # weekly timetable
 
-driver.get('https://ruz.hse.ru/ruz/main')
-sleep(1)
-driver.find_element(By.XPATH, '//button[text()=" Студент "]').click()
-driver.find_elements_by_id("autocomplete-student")[0].click()
-personname = driver.find_elements_by_id("autocomplete-student")[0]
-personname.send_keys('Кокова Полина Дмитриевна')
-sleep(1)
-personname.send_keys(Keys.ENTER)
-sleep(3)
+for person in people:
 
-source = driver.page_source
-soup = BeautifulSoup(source, 'lxml')
+    week = []
+    driver.get('https://ruz.hse.ru/ruz/main')
+    sleep(1)
 
-lessons = soup.find_all('div', class_='media-body')
-#print(lessons[0]
-for lesson in lessons:
-    tmp = BeautifulSoup(lesson, 'lxml')
-    
+    xbutt = driver.find_elements_by_xpath("//button[@class='btn btn-outline-danger mt-3 clear-filter']")[0].click()
+    driver.find_element(By.XPATH, '//button[text()=" Студент "]').click()
+    personname = driver.find_elements_by_id("autocomplete-student")[0]
+    personname.send_keys(person)
+    sleep(1)
+    personname.send_keys(Keys.ENTER)
+    sleep(1)
+    driver.find_element(By.XPATH, '//button[text()=" Список "]').click()
+    sleep(2)
+
+    source = driver.page_source
+    soup = BeautifulSoup(source, 'lxml')
+
+    lessons = soup.find_all('div', class_='media day ng-star-inserted')
+
+    # lets add every пара to weekly timetable of the student
+
+    for i in range(len(lessons)):
+
+        soup = BeautifulSoup(str(lessons[i]), 'lxml')
+        ex = exercise()
+        try:
+            ex.date = soup.find_all('div', class_='day')[0].get_text() + " " + \
+                      soup.find_all('div', class_='month')[0].get_text()  # the day_month
+            ex.day = soup.find_all('div', class_='week')[0].get_text()  # day of the week
+        except IndexError:
+            ex.date = week[-1].date  # the day_month
+            ex.day = week[-1].day  # day of the week
+        ex.time = soup.find_all('div', class_='time')[0].get_text()  # time
+        ex.number = soup.find_all('small')[0].get_text()
+        ex.location = soup.find_all('span', class_='auditorium')[0].get_text() + " " + \
+                      soup.find_all('span', class_='mr-2 text-muted ng-star-inserted')[0].get_text()
+        ex.subject = soup.find_all('span', class_='ng-star-inserted')[0].get_text()
+        ex.type = soup.find_all('div', class_='text-muted kind ng-star-inserted')[0].get_text()
+        ex.group = soup.find_all('div', class_='group ng-star-inserted')[0].get_text()
+        ex.teacher = soup.find_all('div', class_='lecturer')[0].get_text()
+
+        week.append(ex)
+
+    # lets add every ex from weekly timetable to week_dict
+
+    week_dict = dict(zip(weekly_keys, [''] * len(weekly_keys)))
+
+    for elem in week:
+        week_dict[elem.day + elem.number[0]] = str(elem)
+
+    arr.append(week_dict)
+
+df = pd.DataFrame(arr)
+df.index = people
+
+my_date = datetime.date.today()
+year, week_num, day_of_week = my_date.isocalendar()
+
+writer = pd.ExcelWriter('output.xlsx', engine='xlsxwriter')
+df.to_excel(writer, index=False, header=f'Week №{week_num}', sheet_name='Timetable')
+workbook = writer.book
+worksheet = writer.sheets['report']
+
 
 sleep(10)
 
-#driver.close()
+driver.close()
